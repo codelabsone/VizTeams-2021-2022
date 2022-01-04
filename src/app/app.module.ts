1aa8300365844fb81035fba2a03a3f92bc0e38a2
sirSlappz <tamestranger43@gmail.com>
2022-01-03 18:11:34 -0600

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MaterialsModule } from './materials/materials.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MaterialsModule } from './materials/materials.module';
   ],
   imports: [
     BrowserModule,
-    MaterialsModule
+    MaterialsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
