482dad644d98e92c077905d239cd507d1685bacc
sirSlappz <tamestranger43@gmail.com>
2022-01-03 18:02:23 -0600

    other comits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MaterialsModule } from './materials/materials.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+const MatMods: any[] = [
+
+]
+
+
+@NgModule({
+  imports:
+    MatMods
+  ,
+  exports:
+    MatMods
+
+})
+export class MaterialsModule { }
