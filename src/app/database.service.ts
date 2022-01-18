5883f74f53eb6a1793cdb577d7b126ac6933d44a
Spencer B <Cendeu@gmail.com>
2022-01-17 23:03:54 -0600

    fix: :bug: add unused function
    added a function that does nothing just to make the code compile without actually changing anyone's code. This isn't important, it's just to make the website not break until thursday

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -9,7 +9,6 @@ import { Team } from './shared/team.model';
   providedIn: 'root',
 })
 export class DatabaseService {
-
   teams: ReplaySubject<Team[]> = new ReplaySubject(1);
   teamsURL = 'https://vizteams-api.herokuapp.com/teams';
   membersURL = 'https://vizteams-api.herokuapp.com/members';
@@ -18,20 +17,25 @@ export class DatabaseService {
     this.getAllTeams().subscribe((teams) => this.teams.next(teams));
   }
 
-  getMemberById(id: number){
-    return this.http.get<Member>(this.membersURL +'/'+ id)
+  getMemberById(id: number) {
+    return this.http.get<Member>(this.membersURL + '/' + id);
   }
 
   getAllTeams() {
     return this.http.get<Team[]>(this.teamsURL);
   }
 
-  addTeam(team){
-    this.http.post<Team>(this.teamsURL, team).subscribe(
-      () => this.getAllTeams().subscribe((teams) => this.teams.next(teams)))
+  addTeam(team) {
+    this.http
+      .post<Team>(this.teamsURL, team)
+      .subscribe(() =>
+        this.getAllTeams().subscribe((teams) => this.teams.next(teams))
+      );
   }
 
   getTeam(id: number) {
-    return this.http.get<Team>(this.teamsURL + "/" + id)
+    return this.http.get<Team>(this.teamsURL + '/' + id);
   }
+
+  addMember(newMember) {}
 }
